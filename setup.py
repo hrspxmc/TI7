80c4bfca7d3d0a0792c5c3f7056b5cd26245b924
hrspxmc <bklimowski1@gmail.com>
2022-12-31 13:53:33 +0100

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='yachts_webpage',
+    packages=['yachts_webpage'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
+
---
+++ yachs_webpage/views.py
@@ -0,0 +1,6 @@
+from yachs_webpage import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template("base.html", title="Jinja and Flask")
