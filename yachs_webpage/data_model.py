2f5a708b7f47a8114c7fc58d7260695a01987f0c
hrspxmc <bklimowski1@gmail.com>
2023-01-02 22:38:04 +0100

    final

--- yachs_webpage/data_model.py
+++ yachs_webpage/data_model.py
@@ -1,6 +1,7 @@
 from yachs_webpage import db, app
 import datetime
 
+
 class YachtType(db.Model):
     __tablename__ = "YachtTypes"
     id = db.Column(db.Integer, primary_key=True)
@@ -13,55 +14,186 @@ class YachtType(db.Model):
     draft = db.Column(db.Float)
     first_built = db.Column(db.DateTime)
 
+
 class Yacht(db.Model):
     __tablename__ = "Yachts"
     id = db.Column(db.Integer, primary_key=True)
     type_id = db.Column(db.Integer, db.ForeignKey("YachtTypes.id"))
-    unit_type = db.relationship('YachtType', foreign_keys = type_id)
+    unit_type = db.relationship("YachtType", foreign_keys=type_id)
     unit_name = db.Column(db.String)
     built_date = db.Column(db.DateTime)
+
 
 class RentalDate(db.Model):
     __tablename__ = "RentalDates"
     id = db.Column(db.Integer, primary_key=True)
     unit_id = db.Column(db.Integer, db.ForeignKey("Yachts.id"))
-    unit = db.relationship('Yacht', backref=db.backref('rental_date'))
+    unit = db.relationship("Yacht", backref=db.backref("rental_date"))
     booked_from = db.Column(db.DateTime)
     booked_to = db.Column(db.DateTime)
 
-yacht_1 = YachtType(
-    name = "ALLURES 51.9",
-    description = "fine yacht",
-    hull_type = "Keel/CB w/dual rudders",
-    rigging_type = "Cutter",
-    length_overall = 16.67,
-    beam = 4.82,
-    draft = 2.92,
-    first_built = datetime.date(2021, 1, 1)
-)
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
-unit_1 = Yacht(
-    unit_type = yacht_1,
-    unit_name = "Titanic3",
-    built_date = datetime.date(2022, 1, 1)
-)
 
-unit_2 = Yacht(
-    unit_type = yacht_1,
-    unit_name = "Titanic5",
-    built_date = datetime.date(2022, 2, 1)
-)
+yachts = [
+    YachtType(
+        name="Verne",
+        description="fine yacht",
+        hull_type="Keel/CB w/dual rudders",
+        rigging_type="Kuter",
+        length_overall=16.67,
+        beam=3.82,
+        draft=2.92,
+        first_built=datetime.date(2021, 1, 1),
+    ),
+    YachtType(
+        name="Supremator",
+        description="fine yacht",
+        hull_type="Keel/CB w/dual rudders",
+        rigging_type="Galera",
+        length_overall=11.67,
+        beam=4.62,
+        draft=2.22,
+        first_built=datetime.date(2021, 1, 1),
+    ),
+    YachtType(
+        name="Papaj",
+        description="fine yacht",
+        hull_type="Keel/CB w/dual rudders",
+        rigging_type="Krążownik",
+        length_overall=12.67,
+        beam=4.62,
+        draft=2.73,
+        first_built=datetime.date(2021, 1, 1),
+    ),
+    YachtType(
+        name="Nemo",
+        description="fine yacht",
+        hull_type="Keel/CB w/dual rudders",
+        rigging_type="Lotniskowiec",
+        length_overall=26.67,
+        beam=8.82,
+        draft=6.92,
+        first_built=datetime.date(2021, 1, 1),
+    ),
+    YachtType(
+        name="Marszałek",
+        description="fine yacht",
+        hull_type="Keel/CB w/dual rudders",
+        rigging_type="Fregata",
+        length_overall=22.67,
+        beam=21.82,
+        draft=5.92,
+        first_built=datetime.date(2021, 1, 1),
+    ),
+]
 
-rt_date = RentalDate(
-    unit= unit_1,
-    booked_from = datetime.date(2021, 1, 1),
-    booked_to = datetime.date(2021, 2, 1)
-)
+units = [
+    Yacht(unit_type=yachts[0], unit_name="Karol I", built_date=datetime.date(2021, 1, 1)),
+    Yacht(unit_type=yachts[0], unit_name="Jan II", built_date=datetime.date(2021, 1, 1)),
+    Yacht(unit_type=yachts[1], unit_name="Agata IV", built_date=datetime.date(2021, 1, 1)),
+    Yacht(unit_type=yachts[1], unit_name="Wojtek X", built_date=datetime.date(2021, 1, 1)),
+    Yacht(unit_type=yachts[2], unit_name="Zosia V", built_date=datetime.date(2021, 1, 1)),
+    Yacht(unit_type=yachts[3], unit_name="Asia VI", built_date=datetime.date(2021, 1, 1)),
+    Yacht(
+        unit_type=yachts[4], unit_name="Borubar IX", built_date=datetime.date(2021, 1, 1)
+    ),
+    Yacht(unit_type=yachts[4], unit_name="Wojnar XI", built_date=datetime.date(2021, 1, 1)),
+]
+
+rental_dates = [
+    RentalDate(
+        unit=units[0],
+        booked_from=datetime.date(2021, 2, 3),
+        booked_to=datetime.date(2021, 2, 8),
+    ),
+    RentalDate(
+        unit=units[1],
+        booked_from=datetime.date(2021, 1, 3),
+        booked_to=datetime.date(2021, 2, 8),
+    ),
+    RentalDate(
+        unit=units[2],
+        booked_from=datetime.date(2021, 1, 3),
+        booked_to=datetime.date(2021, 1, 23),
+    ),
+    RentalDate(
+        unit=units[3],
+        booked_from=datetime.date(2021, 1, 13),
+        booked_to=datetime.date(2021, 3, 2),
+    ),
+    RentalDate(
+        unit=units[4],
+        booked_from=datetime.date(2021, 2, 3),
+        booked_to=datetime.date(2021, 2, 22),
+    ),
+    RentalDate(
+        unit=units[5],
+        booked_from=datetime.date(2021, 2, 3),
+        booked_to=datetime.date(2021, 3, 1),
+    ),
+    RentalDate(
+        unit=units[6],
+        booked_from=datetime.date(2021, 1, 3),
+        booked_to=datetime.date(2021, 2, 6),
+    ),
+    RentalDate(
+        unit=units[7],
+        booked_from=datetime.date(2021, 2, 3),
+        booked_to=datetime.date(2021, 2, 8),
+    ),
+    RentalDate(
+        unit=units[7],
+        booked_from=datetime.date(2021, 3, 3),
+        booked_to=datetime.date(2021, 4, 8),
+    ),
+    RentalDate(
+        unit=units[1],
+        booked_from=datetime.date(2021, 3, 31),
+        booked_to=datetime.date(2021, 5, 1),
+    ),
+    RentalDate(
+        unit=units[0],
+        booked_from=datetime.date(2021, 3, 13),
+        booked_to=datetime.date(2021, 4, 8),
+    ),
+    RentalDate(
+        unit=units[1],
+        booked_from=datetime.date(2021, 3, 5),
+        booked_to=datetime.date(2021, 3, 10),
+    ),
+    RentalDate(
+        unit=units[2],
+        booked_from=datetime.date(2021, 3, 13),
+        booked_to=datetime.date(2021, 3, 28),
+    ),
+    RentalDate(
+        unit=units[3],
+        booked_from=datetime.date(2021, 3, 11),
+        booked_to=datetime.date(2021, 3, 18),
+    ),
+    RentalDate(
+        unit=units[4],
+        booked_from=datetime.date(2021, 3, 16),
+        booked_to=datetime.date(2021, 4, 18),
+    ),
+    RentalDate(
+        unit=units[5],
+        booked_from=datetime.date(2021, 3, 3),
+        booked_to=datetime.date(2021, 4, 18),
+    ),
+    RentalDate(
+        unit=units[6],
+        booked_from=datetime.date(2021, 3, 1),
+        booked_to=datetime.date(2021, 5, 12),
+    )
+]
+
 
 with app.app_context():
     db.create_all()
-    db.session.add(yacht_1)
-    db.session.add(unit_1)
-    db.session.add(unit_2)
-    db.session.add(rt_date)
+    [db.session.add(x) for x in yachts]
+    [db.session.add(x) for x in units]
+    [db.session.add(x) for x in rental_dates]
     db.session.commit()
--- yachs_webpage/views.py
+++ yachs_webpage/views.py
@@ -2,22 +2,50 @@ from yachs_webpage import app
 from flask import render_template
 import yachs_webpage.data_model as data_model
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/yachts')
-def yachts():
-    return render_template("yachts.html", yachts = data_model.YachtType.query.all()) 
 
-@app.route('/contact')
+@app.route("/yachts")
+def yachts():
+    return render_template("yachts.html", yachts=data_model.YachtType.query.all())
+
+
+@app.route("/contact")
 def contact():
     return render_template("contact.html")
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
     return render_template("index.html")
-    
-@app.route('/calendar')
+
+
+@app.route("/calendar")
 def calendar():
-    return render_template("calendar.html") 
+
+    rental_dates = data_model.RentalDate.query.all()
+    yacht_models = data_model.YachtType.query.all()
+
+    booked_dates = [
+        {"id": xi,
+        "group": int(x.unit.unit_type.id),
+         "start": x.booked_from.strftime('%Y-%m-%d'),
+         "end": x.booked_to.strftime('%Y-%m-%d'),
+         "type": "range",
+         "content": x.unit.unit_name}
+        for xi, x in enumerate(rental_dates)
+    ]
+
+    groups_list = [
+        {"id": int(x.id),
+        "content": str(x.name)}
+        for x in yacht_models 
+    ]
+
+
+    #return booked_dates
+    #return groups_list
+    return render_template("calendar.html", dates = booked_dates, groups = groups_list)
